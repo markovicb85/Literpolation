45a402c8c35da81ff799073935ac995e862e9842
crni <markovicb85@gmail.com>
2018-08-20 22:09:49 +0200

    Radna verzija aplikacije

--- app/src/main/java/com/example/crni/literpolation/MainActivity.java
+++ app/src/main/java/com/example/crni/literpolation/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.crni.literpolation;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -9,16 +11,21 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends Activity {
 
+    private static int startPosition = 0;
     private String number = "";
     private TextView display;
-    private TextView x1, x, x2;
-    private TextView y1, y, y2;
-    private String position = "x1";
-    private double result;
-    private double x_0, x_1, x_2;
-    private double y_1, y_2;
+    private TextView tv_1, tv_2, tv_3;
+    private TextView tv_4, tv_5, tv_6;
+
+    private double[] values = new double[6];
+    private ArrayList<TextView> tv = new ArrayList<>();
 
 
     @Override
@@ -28,12 +35,14 @@ public class MainActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         display = (TextView) findViewById(R.id.textView);
-        x1 = (TextView) findViewById(R.id.et_x1);
-        x = (TextView) findViewById(R.id.et_x2);
-        x2 = (TextView) findViewById(R.id.et_x3);
-        y1 = (TextView) findViewById(R.id.et_y1);
-        y = (TextView) findViewById(R.id.et_y2);
-        y2 = (TextView) findViewById(R.id.et_y3);
+        tv_1 = (TextView) findViewById(R.id.et_x1);
+        tv_2 = (TextView) findViewById(R.id.et_x2);
+        tv_3 = (TextView) findViewById(R.id.et_x3);
+        tv_4 = (TextView) findViewById(R.id.et_y1);
+        tv_5 = (TextView) findViewById(R.id.et_y2);
+        tv_6 = (TextView) findViewById(R.id.et_y3);
+
+        tv.addAll(Arrays.asList(tv_1, tv_2, tv_3, tv_4, tv_6));
     }
 
     @Override
@@ -43,11 +52,28 @@ public class MainActivity extends Activity {
 
     public void onClickNumber(View v){
         Button b = (Button) v;
-        if(b.getText().equals("-") && number.length() > 0){
-            //Ne radi nista
-        }else{
-            number += b.getText();
-            updateDisplay(number);
+        number += b.getText();
+        switch (number){
+            case "0":
+                updateDisplay(number);
+                break;
+            case ".":
+                updateDisplay(number);
+                break;
+            case "-":
+                updateDisplay(number);
+                break;
+            case "00":
+                number = "0";
+                updateDisplay(number);
+                break;
+            default:
+                if(NumberUtils.isCreatable(number)){
+                    updateDisplay(number);
+                }else{
+                    number = number.substring(0, number.length() - 1);
+                }
+                break;
         }
     }
 
@@ -57,7 +83,8 @@ public class MainActivity extends Activity {
 
     private void clearDisplay(){
         number = "";
-        display.setText("0");
+        display.setText("");
+        display.setHint("0");
     }
 
     public void onClickClear(View v){
@@ -69,78 +96,109 @@ public class MainActivity extends Activity {
         }
     }
 
-    public void onClickEqual(View v){
-        if(number.equals("") || number.equals(".") || number.equals("-")){
+    public void onClickEqual(View v, int position){
+
+        boolean validation = false;
+
+        if(number.equals("")){
             number = "0";
-        }
-        switch (position){
-            case "x1":
-                x1.setText(number);
-                x1.setTextColor(Color.BLACK);
-                x_1 = Double.parseDouble(number);
-                clearDisplay();
-                position = "x2";
-                break;
-            case "x2":
-                x.setText(number);
-                x.setTextColor(Color.BLACK);
-                x_0 = Double.parseDouble(number);
-                clearDisplay();
-                position = "x3";
-                break;
-            case "x3":
-                x2.setText(number);
-                x2.setTextColor(Color.BLACK);
-                x_2 = Double.parseDouble(number);
-                clearDisplay();
-                position = "y1";
-                break;
-            case "y1":
-                y1.setText(number);
-                y1.setTextColor(Color.BLACK);
-                y_1 = Double.parseDouble(number);
-                clearDisplay();
-                position = "y2";
-                break;
-            case "y2":
-                y2.setText(number);
-                y2.setTextColor(Color.BLACK);
-                y_2 = Double.parseDouble(number);
-                clearDisplay();
-                position = "y3";
-                break;
-            default:
-                result = calculation();
-                y.setTextColor(Color.BLACK);
-                y.setText(Double.toString(result));
-                position = "x1";
-                break;
+        }else if(number.equals(".") || number.equals("-")){
+            number = "";
         }
 
+
+        setHintTextColor();
+
+        for (int i = position; i < tv.size(); i++) {
+            if (tv.get(i).getText() == ""){
+                tv.get(i).setText(number);
+                clearDisplay();
+
+                for (TextView item : tv) {
+                    if(item.getText().equals("")){
+                        validation = false;
+                        break;
+                    }else{ validation = true; }
+                }
+                if(validation){
+                    calculation(tv);
+                    tv_5.setTextColor(Color.RED);
+                }
+                break;
+            }
+        }
+        clearDisplay();
     }
 
-    public void onClickSelect(View v){
-        TextView tv= (TextView) v;
-        number = (String) tv.getText();
-        updateDisplay(number);
-        tv.setTextColor(Color.RED);
-        position = (String) tv.getTag();
+    public void onClickEqual(View view){
+        onClickEqual(view, startPosition);
     }
 
-    private double calculation() {
-        result = y_1 - (x_1-x_0)*(y_1-y_2)/(x_1-x_2);
-        return result;
+    public void onClickSelect(View view){
+
+        if(view.getTag() != "6"){
+            TextView tv = (TextView) view;
+            startPosition = Integer.parseInt(tv.getTag().toString()) - 1;
+            clearDisplay();
+            tv.setText("");
+            tv.setHintTextColor(Color.RED);
+            tv_5.setTextColor(Color.GRAY);
+        }
+    }
+
+    public void showDialogResult(View view){
+        AlertDialog.Builder myBulder = new AlertDialog.Builder(MainActivity.this);
+        @SuppressLint("InflateParams")
+        View myView = getLayoutInflater().inflate(R.layout.dialog_info, null);
+
+        TextView x1_value = (TextView) myView.findViewById(R.id.x1_value);
+        TextView x2_value = (TextView) myView.findViewById(R.id.x2_value);
+        TextView x3_value = (TextView) myView.findViewById(R.id.x3_value);
+        TextView y1_value = (TextView) myView.findViewById(R.id.y1_value);
+        TextView y2_value = (TextView) myView.findViewById(R.id.y2_value);
+        TextView y3_value = (TextView) myView.findViewById(R.id.y3_value);
+
+        x1_value.setText(tv_1.getText());
+        x2_value.setText(tv_2.getText());
+        x3_value.setText(tv_3.getText());
+        y1_value.setText(tv_4.getText());
+        y2_value.setText(tv_5.getText());
+        y3_value.setText(tv_6.getText());
+
+        myBulder.setView(myView);
+        AlertDialog dialog = myBulder.create();
+        dialog.show();
+    }
+
+    private void calculation(ArrayList<TextView> list){
+        int i = 0;
+        for (TextView v : list) {
+            values[i] = Double.parseDouble((String) v.getText());
+            i++;
+        }
+        double result = values[3] - (values[0] - values[1])*(values[3] - values[4])/(values[0] - values[2]);
+        tv_5.setText(String.valueOf(result));
     }
 
     public void onClickClearAll(View v){
-        x1.setText(null);
-        x.setText(null);
-        x2.setText(null);
-        y1.setText(null);
-        y.setText(null);
-        y2.setText(null);
+        tv_1.setText(null);
+        tv_2.setText(null);
+        tv_3.setText(null);
+        tv_4.setText(null);
+        tv_5.setText(null);
+        tv_6.setText(null);
         display.setText("0");
         number = "";
-        position = "x1";
+        startPosition = 0;
+        setHintTextColor();
     }
+
+    public void setHintTextColor(){
+        for (TextView text : tv) {
+            text.setHintTextColor(Color.GRAY);
+        }
+        tv_5.setHintTextColor(Color.GRAY);
+    }
+
+    //TODO Blokirati digitron kada su sva polja popunjena ili ga vratioti na prvo...
 }
